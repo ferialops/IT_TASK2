ad1981d998debf8938bb2cd5c5eb6635745b19e5
ferialops <90615135+ferialops@users.noreply.github.com>
2024-06-03 15:55:58 +0300

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+using Task2.ViewModels;
+
+namespace Task2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new MainViewModel();
+        }
+
+    }
+}
---
+++ Models/ОсветительныйПрибор.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Task2.Models
+{
+    public abstract class ОсветительныйПрибор
+    {
+        public bool Включено { get; protected set; }
+        public event EventHandler Сломан;
+
+        public abstract void Включить();
+        public abstract void Выключить();
+
+        protected void Сломать()
+        {
+            Включено = false;
+            Сломан?.Invoke(this, EventArgs.Empty);
+            Console.WriteLine("Прибор сломан.");
+        }
+    }
+}
